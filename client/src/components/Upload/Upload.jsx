a60592f298c398b4512b6be5d20302896dedd268
webHikari <q5503212@gmail.com>
2024-04-14 05:33:46 +0300

    124

--- client/src/components/Upload/Upload.jsx
+++ client/src/components/Upload/Upload.jsx
@@ -1,9 +1,6 @@
-import { useState, useRef, useEffect } from 'react';
-import CSVReader from 'react-csv-reader';
+import { useState, useRef } from 'react';
 
 import styles from './Upload.module.css';
-
-import URL from '../../config/URL.jsx'
 
 export default function Upload() {
   // State variables
@@ -15,36 +12,39 @@ export default function Upload() {
   const fileInputRef = useRef(null);
 
   // Function to handle file selection from input
-  const handleFileChange = (data, fileInfo) => {
-    // File data is available in 'data' variable
-    console.log('Parsed CSV data:', data);
-    console.log('File info:', fileInfo);
-    handleUpload(data);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    handleUpload(file);
   };
 
   // Function to handle file upload
-  const handleUpload = async (fileData) => {
+  const handleUpload = async (file) => {
     setUploading(true); // Set uploading to true
 
     try {
-      // Perform file upload operation here, using 'fileData'
-      // Replace '/upload' with the actual upload endpoint
-      const jsonData = { data: fileData };
+      const formData = new FormData();
+      formData.append('file', file); // Append the file to FormData
 
       const response = await fetch("http://62.113.115.130:8000/predict/", {
         method: 'POST',
-        body: JSON.stringify(jsonData), // Send parsed CSV data as JSON
-        headers: {
-          'Content-Type': 'application/json'
-        },
+        body: formData, // Send FormData containing the file
         onUploadProgress: (progressEvent) => {
           const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
           setUploadProgress(progress);
         },
       });
-      console.log(response);
+
       if (response.ok) {
         setUploadStatus('Upload success');
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'output.csv'; // Имя файла, который будет загружен
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
       } else {
         throw new Error('Upload failed');
       }
@@ -67,7 +67,7 @@ export default function Upload() {
       onClick={openFileDialog}
       onDrop={(e) => {
         e.preventDefault();
-        handleFileChange(e.dataTransfer.files[0]);
+        handleFileChange(e);
       }}
       onDragOver={(e) => e.preventDefault()}
       className={styles.Upload}
@@ -75,16 +75,11 @@ export default function Upload() {
       <p>Click or Drag & Drop to Select File</p>
       {uploading && <p>Uploading... {uploadProgress}%</p>}
       {uploadStatus && <p>{uploadStatus}</p>}
-      <CSVReader 
-
-        onFileLoaded={handleFileChange}
+      <input
+        type="file"
         ref={fileInputRef}
-        parserOptions={{
-          header: true,
-          dynamicTyping: true,
-          skipEmptyLines: true,
-          transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
-        }}
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
       />
     </div>
   );
